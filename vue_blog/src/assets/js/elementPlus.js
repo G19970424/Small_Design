5c7eab5a693014362d66a83e845a731c84e05dee
JiaJu Ge <53358092+G19970424@users.noreply.github.com>
2024-07-01 21:57:23 +0800

    Delete vue_blog directory

--- vue_blog/src/assets/js/elementPlus.js
+++
@@ -1,8 +0,0 @@
-import { ElButton, ElAlert, ElForm, ElInput, ElFormItem } from 'element-plus'
-export default(app)=>{
-    app.use(ElButton)
-    app.use(ElAlert)
-    app.use(ElForm)
-    app.use(ElFormItem)
-    app.use(ElInput)
-}
--- vue_blog/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-//import 'element-plus/lib/theme-chalk/index.css'
-import router from "./router"
-import 'animate.css'
-let app = createApp(App)
-import eleP from'./assets/js/elementPlus'
-eleP(app)
-app.use(router).mount('#app' )
